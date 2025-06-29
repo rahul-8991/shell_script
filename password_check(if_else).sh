a5539ba3cda2141ce662f4b90f790ae12b922327
unknown <kudikyalarahul8055@gmail.com>
2025-06-29 23:25:18 +0530

    password check script with if else condition

--- password_check(if_else).sh
+++ password_check(if_else).sh
@@ -1,25 +1,30 @@
 #!/bin/bash
 
-# Ask user to enter password
-read -sp "Enter your password: " password
-echo $password
-
 echo "Minimum password requirements"
 echo "At least 8 characters, 1 uppercase, 1 lowercase, 1 digit, 1 special character"
 
-if [[ ${#password} -lt 8 ]]; then
+# Prompt user for password (silent input)
+read -sp "Enter your password: " password
+echo
+
+# Check length
+if [ ${#password} -lt 8 ]; then
     echo "❌ Password must be at least 8 characters long."
 
+# Check for at least one uppercase letter
 elif ! [[ "$password" =~ [A-Z] ]]; then
     echo "❌ Password must contain at least one uppercase letter."
 
+# Check for at least one lowercase letter
 elif ! [[ "$password" =~ [a-z] ]]; then
     echo "❌ Password must contain at least one lowercase letter."
 
+# Check for at least one digit
 elif ! [[ "$password" =~ [0-9] ]]; then
     echo "❌ Password must contain at least one digit."
 
-elif ! [[ "$password" =~ [\@\#\$\%\^\&\*\!\_\-\+] ]]; then
+# Check for at least one special character (from a safe set)
+elif ! [[ "$password" =~ [@#$%^&*!_\-+=] ]]; then
     echo "❌ Password must contain at least one special character (@, #, $, %, etc.)."
 
 else
