986da96a85e1b1cbcc7e522c42a46628f114e1fa
unknown <kudikyalarahul8055@gmail.com>
2025-06-29 23:05:08 +0530

    updates special variables script

--- special variables.sh
+++ special variables.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+echo "All variables passed to the script: $@"
+echo "Number of variables: $#"
 echo "Script name: $0"
-echo "First argument: $1"
-echo "All arguments: $@"
-echo "Number of arguments: $#"
-echo "Last exit status: $?"
-echo "Script PID: $$"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
